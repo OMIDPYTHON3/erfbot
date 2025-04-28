59d76c232bac10454b2013f14268bccd255d3f98
OMIDPYTHON3 <omidhacker200@gmail.com>
2025-04-28 07:22:14 +0330

    ad

--- botsaz.py
+++ botsaz.py
@@ -10,29 +10,29 @@ import tgcrypto
 server = Flask(__name__)
 
 # اطلاعات ربات
-ONWER = [2029106808,5861650867]
+ONWER = {2029106808, 5861650867}  # ست استفاده کردم بهتره
 API_HASH = "8edc44bad592cbc7772df91460cce721"
 API_ID = 17876846
-bot = Client(name="botsaz", bot_token="5892949239:AAFLC319sZ_t9INl6OV29wWooce5dl0TFz8", api_hash=API_HASH, api_id=API_ID)
+bot = Client(name="botsaz", bot_token="5892949239:AAFLC319sZ_t9INl6OV29wWooce5dl0TFz8", api_hash=API_HASH, api_id=API_ID, in_memory=True)
 
 # هندلرها
 @bot.on_message(filters.command("start"))
 @bot.on_message(filters.regex("بازگشت"))
 async def Start(client, message):
-    if message.from_user.id == ONWER:
+    if message.from_user.id in ONWER:
         reply = ReplyKeyboardMarkup(keyboard=[["ساخت ربات"]], resize_keyboard=True)
-        await bot.send_message(chat_id=message.chat.id, text="خوش آمدید، انتخاب کنید", reply_markup=reply)
+        await message.reply("خوش آمدید، انتخاب کنید", reply_markup=reply)
 
 @bot.on_message(filters.regex("ساخت ربات"))
 async def Create(client, message):
-    if message.from_user.id == ONWER:
+    if message.from_user.id in ONWER:
         reply = ReplyKeyboardMarkup(keyboard=[["بازگشت"]], resize_keyboard=True)
-        token = await bot.ask(chat_id=message.chat.id, text="توکن ربات را ارسال کنید", reply_markup=reply, filters=filters.user(message.from_user.id))
+        token = await message.chat.ask("توکن ربات را ارسال کنید", reply_markup=reply, filters=filters.user(message.from_user.id))
         if token.text == "بازگشت":
             await Start(client, token)
         else:
             for i in token.text.splitlines():
-                await bot.send_message(chat_id=message.chat.id, text="ربات فعال شد", reply_markup=ReplyKeyboardMarkup(keyboard=[["ساخت ربات"]], resize_keyboard=True))
+                await message.reply("ربات فعال شد", reply_markup=ReplyKeyboardMarkup(keyboard=[["ساخت ربات"]], resize_keyboard=True))
                 attacker = Bot()
                 await attacker.attacker(name=i.split(":")[0], token=token.text)
 
